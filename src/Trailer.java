1563deda9af14f1e7f3846f67e7c33e6a2adf156
Matagaro <nyahumphrey@gmail.com>
2024-01-19 10:17:14 +0300

    created trailer class and all its attributes

---
+++ src/Trailer.java
@@ -0,0 +1,43 @@
+public class Trailer extends Vehicle{
+
+    String competitiveSport,engineCapacity,computerGame;
+
+    public Trailer(String type, String numberPassengers, String propulsionTechnology, String competitiveSport, String engineCapacity, String computerGame) {
+        super(type, numberPassengers, propulsionTechnology);
+        this.competitiveSport = competitiveSport;
+        this.engineCapacity = engineCapacity;
+        this.computerGame = computerGame;
+    }
+
+    public String getCompetitiveSport() {
+        return competitiveSport;
+    }
+
+    public void setCompetitiveSport(String competitiveSport) {
+        this.competitiveSport = competitiveSport;
+    }
+
+    public String getEngineCapacity() {
+        return engineCapacity;
+    }
+
+    public void setEngineCapacity(String engineCapacity) {
+        this.engineCapacity = engineCapacity;
+    }
+
+    public String getComputerGame() {
+        return computerGame;
+    }
+
+    public void setComputerGame(String computerGame) {
+        this.computerGame = computerGame;
+    }
+
+    @Override
+    public void vehicleCharacteristics() {
+        super.vehicleCharacteristics();
+
+        System.out.println("Competitive Sport: "+competitiveSport+" Engine Capacity: "+engineCapacity+" Computer Game: "+computerGame);
+
+    }
+}
